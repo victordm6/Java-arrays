100a99e7fa415cce15b40b0fa486ee5201471d10
victordm6 <victor.dm6@hotmail.com>
2022-06-27 21:57:18 -0300

    :tada: Commit inicial

---
+++ src/OrdemInversa.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class OrdemInversa {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        double numero;
+    
+        System.out.print("Numeros: ");
+        numero = teclado.nextDouble();
+    }
+}
